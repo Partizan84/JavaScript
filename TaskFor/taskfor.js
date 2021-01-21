f0b08bc183177b81df7c28dfa272865b617cb859
Partizan84 <knazarov84@gmail.com>
2021-01-22 00:27:13 +0500

    Finaly update TaskThree.

--- TaskFor/taskfor.js
+++ TaskFor/taskfor.js
@@ -1,10 +1,46 @@
 'use strict';
 let a = 2, b = 5;
+/**
+ * Функция складывает переданные параметры.
+ * @param {*} a 
+ * @param {*} b 
+ */
 function Summa(a,b){
+    let c;
     c = a + b;
     return console.log(c);
 }
-function Raznost(a,b){
+/**
+ * Функция вычитает переданные параметры.
+ * @param {*} a 
+ * @param {*} b 
+ */
+function Difference(a,b){
+    let c;
     c = a - b;
     return console.log(c);
 }
+/**
+ * Функция делит переданные параметры.
+ * @param {*} a 
+ * @param {*} b 
+ */
+function Division(a,b){
+    let c;
+    c = a / b;
+    return console.log(c);
+}
+/**
+ * Функция перемножает переданные параметры.
+ * @param {*} a 
+ * @param {*} b 
+ */
+function Multiplication(a,b){
+    let c;
+    c = a * b;
+    return console.log(c);
+}
+Summa(a,b);
+Difference(a,b);
+Division(a,b);
+Multiplication(a,b);
